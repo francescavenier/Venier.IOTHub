54ce782a3448111ace188dd12cb52bb6a1fd6215
francescavenier <francescavenier10@gmail.com>
2020-01-15 16:39:29 +0100

    Send message

--- Venier.IOTHub.WebApp/Pages/Index.cshtml.cs
+++ Venier.IOTHub.WebApp/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,10 +15,16 @@ namespace Venier.IOTHub.WebApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly IConfiguration _configuration;
-        private readonly ServiceClient _serviceClient;
+        //private readonly ServiceClient _serviceClient;
+        private static TransportType s_transportType = TransportType.Amqp;
 
+        [BindProperty]
         public string message { get; set; }
+
+        [BindProperty]
         public string deviceId { get; set; }
+
+        public string connectionString { get { return _configuration.GetConnectionString("connectionString"); } }
 
         public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
@@ -34,8 +41,10 @@ namespace Venier.IOTHub.WebApp.Pages
         {
             if (ModelState.IsValid)
             {
-                await _serviceClient.SendAsync(deviceId, message).ConfigureAwait(false);
-                return RedirectToPage("/Index"); // da modificare il redirect
+                var BitMessage = new Message(Encoding.ASCII.GetBytes(message));
+                ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(connectionString, s_transportType);
+                serviceClient.SendAsync(deviceId, BitMessage).ConfigureAwait(false);
+                return RedirectToPage("/Confirm"); // da modificare il redirect
             }
             return RedirectToPage("/error404");
 
